94926e54103f1ee4ec392a5714b075f07b3d5636
Frederic Olivier <jpolivierr@gmail.com>
2020-12-18 22:53:21 -0500

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const path = require('path')
 const app = express()
+const cors = require('cors')
 const PORT = 5000
 
 //* MIDDLEWARE **//
+app.use(cors())
 app.use(express.json({extended: false}))
 
 
